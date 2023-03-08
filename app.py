517faebd62c7e46adec929911e3b5ed66ba87a60
Arnau Albert <arnaualbert@pop-os.localdomain>
2023-03-08 09:09:48 +0100

    v1

--- app.py
+++ app.py
@@ -66,7 +66,14 @@ def demultiplexing():
                 print(f.filename)
                 fastas_rv_ls.append(os.path.join(filename))
         output_dir = request.form['output_dir']
-        ref_genome = request.form.getlist('ref_genome')
+        # ref_genome = request.form.getlist('ref_genome')
+        ###################################################################
+        ref_genome = request.files.getlist('ref_genome')
+        ref_genome_ls = []
+        for f in ref_genome:
+            filename = secure_filename(f.filename)
+            ref_genome_ls.append(os.path.join(filename))         
+        ##################################################################    
         organism_name = request.form.getlist('organism_name')
         num_of_threads = request.form['num_of_threads']
         reads_per_chunk = request.form['reads_per_chunk']
@@ -83,7 +90,8 @@ def demultiplexing():
         print(type(params))
         fastas_fs_ls_string = " ".join(fastas_fwd_ls)
         fastas_rv_ls_string = " ".join(fastas_rv_ls)
-        ref_genome_string = " ".join(ref_genome)
+        # ref_genome_string = " ".join(ref_genome)
+        ref_genome_string = " ".join(ref_genome_ls)
         organism_name_string = " ".join(organism_name)
         rpl_ls_str = " ".join(rpl_ls)
         command = f'split_pooledSeqWGS_parallel.py --fastq1 {fastas_fs_ls_string} --fastq2 {fastas_rv_ls_string} --outdir {output_dir} --refGenomes {ref_genome_string} --sampleNames {organism_name_string} --trheads {num_of_threads} --nreads_per_chunk {reads_per_chunk} --replace {rpl_ls_str} --skip_removing_tmp_files {skip_removing_tmp_files} --wit_db {wit_db}'
@@ -129,7 +137,7 @@ def demultiplexing_batch():
         skip_removing_tmp_files = request.form['skip_removing_tmp_files']
         wit_db = request.form['wit_db']
         # params = {"fastas_fwd":fastas_fwd_ls,"fastas_rv":fastas_rv_ls,"output_dir":output_dir,"ref_genome":ref_genome,"organism_name":organism_name,"num_of_threads":num_of_threads,"reads_per_chunk":reads_per_chunk,"skip_removing_tmp_files":skip_removing_tmp_files,"wit_db":wit_db}
-        params = {"--fastq1":fastas_fwd_ls,"--fastq2":fastas_rv_ls,"--outdir":output_dir,"--refGenomes":ref_genome,"--sampleNames":organism_name,"--trheads":num_of_threads,"--nreads_per_chunk":reads_per_chunk,"--skip_removing_tmp_files":skip_removing_tmp_files,"--wit_db":wit_db}
+        params = {"split_pooledSeqWGS_parallel.py --fastq1":fastas_fwd_ls,"--fastq2":fastas_rv_ls,"--outdir":output_dir,"--refGenomes":ref_genome,"--sampleNames":organism_name,"--trheads":num_of_threads,"--nreads_per_chunk":reads_per_chunk,"--skip_removing_tmp_files":skip_removing_tmp_files,"--wit_db":wit_db}
         print(params)
         print(type(params))
         #for rout in fastas_fwd_ls:
